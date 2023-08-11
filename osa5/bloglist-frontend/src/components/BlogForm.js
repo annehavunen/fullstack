cd1dbf50b32a32afe4f0c8d93fff08fce6c3f633
Anne Havunen <anne.havunen@helsinki.fi>
2023-08-11 15:38:40 +0300

    Tee tehtävä 5.12 PropType ja ESLint

--- osa5/bloglist-frontend/src/components/BlogForm.js
+++ osa5/bloglist-frontend/src/components/BlogForm.js
@@ -1,4 +1,5 @@
 import { useState } from 'react'
+import PropTypes from 'prop-types'
 
 const BlogForm = ({ createBlog }) => {
   const [newTitle, setNewTitle] = useState('')
@@ -30,6 +31,10 @@ const BlogForm = ({ createBlog }) => {
       </form>
     </div>
   )
+}
+
+BlogForm.propTypes = {
+  createBlog: PropTypes.func.isRequired
 }
 
 export default BlogForm
