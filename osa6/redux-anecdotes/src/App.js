1b2309af865fb1f1e1b3b26a64b50911996879cb
Anne Havunen <anne.havunen@helsinki.fi>
2023-08-16 12:55:50 +0300

    Tee tehtävät 6.3 - 6.8

---
+++ osa6/redux-anecdotes/src/App.js
@@ -0,0 +1,14 @@
+import AnecdoteForm from "./components/AnecdoteForm"
+import AnecdoteList from "./components/AnecdoteList"
+
+const App = () => {
+  return (
+    <div>
+      <h1>Anecdotes</h1>
+      <AnecdoteForm />
+      <AnecdoteList />
+    </div>
+  )
+}
+
+export default App
